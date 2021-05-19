e089814cd6c92d58ef2f1792d1948fc1f090a45d
Justin Horn <justinhorn0000@gmail.com>
2021-05-19 23:29:45 +0200

    added info endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const ytdl = require("ytdl-core");
 
 const { parseVideo } = require("../download");
 
@@ -17,6 +18,30 @@ router.get("/parseVideo/:videoCode", async function (req, res, next) {
   } catch (e) {
     next(e);
     res.status(400).send("error");
+  }
+});
+
+router.get("/info/:videoCode", async function (req, res, next) {
+  try {
+    const { videoCode } = req.params;
+
+    let info = await ytdl.getInfo(videoCode);
+    let videoDetails = info.videoDetails;
+    let thumbnail = videoDetails.thumbnails[0].url;
+    let title = videoDetails.title;
+    let lengthSeconds = videoDetails.lengthSeconds;
+
+    let audios = info.formats.filter(
+      (x) =>
+        x.mimeType.includes("audio/mp4") || x.mimeType.includes("audio/mp3")
+    );
+
+    let audio = audios.length > 0 ? audios[0].url : undefined;
+
+    const infoData = { title, thumbnail, lengthSeconds, audio };
+    res.json(infoData);
+  } catch (e) {
+    next(e);
   }
 });
 
